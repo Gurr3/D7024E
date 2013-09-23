0ccc71ee4362235cfeaf52fb3080244ccc63fef7
Gurr3 <gurr72@gmail.com>
2013-09-23 14:15:04 +0200

    client can use push(file,url) to connect to the post.js website

--- nodejs_testing_william/helloworldserver.js
+++ nodejs_testing_william/helloworldserver.js
@@ -8,11 +8,16 @@ function execute( func, value){
 	func(value);
 }
 
-http.createServer(onRequest).listen(8888);
+
+if (!module.parent) {
+  http.createServer(onRequest).listen(8888)//app.listen(8080);
+  console.log('Port', 8888);
+}
+//http.createServer(onRequest).listen(8888);
 
 function onRequest (request, response){
-	console.write("Got request");
+	console.log("Got request");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
+	response.write("Hello World!");
 	response.end();
 }
